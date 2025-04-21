464b037f712d25a7ab022f72898d48cb2e6af037
gigapin <gigaprog7@gmail.com>
2025-04-21 15:16:18 +0200

    Initial config

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/sample-data/db/sample-data";
+
+const HomePage = () => {
+  return (
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList title="Newest Arrivals" data={sampleData.products} />
+    </div>
+  );
+};
+export default HomePage;
