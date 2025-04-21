71f4e3e980f523d70e083587aa02e92d47261c22
gigapin <gigaprog7@gmail.com>
2025-04-21 15:38:35 +0200

    Removed seed data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
-import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/sample-data/db/sample-data";
+//import ProductList from "@/components/shared/product/product-list";
+//import sampleData from "@/sample-data/db/sample-data";
 
 const HomePage = () => {
   return (
     <div className="space-y-8">
       <h2 className="h2-bold">Latest Products</h2>
-      <ProductList title="Newest Arrivals" data={sampleData.products} />
+      {/* <ProductList title="Newest Arrivals" data={sampleData.products} /> */}
     </div>
   );
 };
